3c0245a579312962e0eaa8d44717209b7a566b35
Warren LaFrance <lafrance.warren@jcdev.org>
2019-06-21 14:33:37 -0500

    Saved last updated file

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -2,32 +2,31 @@ import axios from 'axios';
 
 const url = 'http://localhost:5000/api/posts/';
 
-
-class PostService{
-    //GET
-static getPosts(){
-     return new Promise(async (resolve,reject)=>{
-    try {
-        const res = await axios.get(url);
-        const data = res.data;
-        resolve(
-            data.map(post => ({
-                ...post,
-                createdAt:new Date(post.createdAt)
-            }))
-        );    
-    } catch (err) {
-        reject(err);
+class PostService {
+    //GET POST
+    static getPosts() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch (err) {
+                reject(err);
+            }
+        })
     }
-})
-}
 
     //CREATE
-static inserPost(text){
-    return axios.post(url,{text});
-}
+    static inserPost(text) {
+        return axios.post(url, { text });
+    }
     //DELETE
-    static deletePost(id){
+    static deletePost(id) {
         return axios.deletePost(`${url}${id}`);
     }
 }
